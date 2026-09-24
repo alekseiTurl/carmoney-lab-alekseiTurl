395435eec2b66d03a50b1cc7e349db298c57c222
Турляков Алексей Александрович <aleskei.turl@gmail.com>
2026-09-24 17:14:03 +0300

    Revert "index.php: add entry point docblock"
    This reverts commit c29d69aa75a8393c7481c953812e3331bec58d02.

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,11 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Точка входа HTTP-приложения: подключаем автозагрузчик Composer
- * и передаём управление собранному в AppFactory Slim-приложению.
- */
-
 use CarMoneyLab\AppFactory;
 
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
